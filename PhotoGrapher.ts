bd49ece62ef98aa7a55892c72321ede0de1a8565
vicky kumar <VICKYMODI951@GMAIL.COM>
2019-08-28 20:10:30 +0530

    Add files via upload

---
+++ PhotoGrapher.ts
@@ -0,0 +1,9 @@
+export interface PhotoGrapher {
+    id:Number,
+    GrapherName:String;
+    Age:Number;
+    Special_IN : String;
+    Location: String;
+    no_of_rating: Number;
+    avg_rating : Number;      
+}
